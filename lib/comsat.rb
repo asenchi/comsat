f20289c5f91f98a6ae7ecc0affb386aed425c219
Michael Gorsuch <michael.gorsuch@gmail.com>
2012-04-10 13:55:32 -0500

    support for resolve

--- lib/comsat.rb
+++ lib/comsat.rb
@@ -84,15 +84,23 @@ module Comsat
       @api_key = uri.user
     end
 
-    def send_notice(data)
+    def contact_pagerduty(event_type, data)
+      id         = data[:message_id] || SecureRandom.uuid
       message    = data[:message]
       source     = data[:source]
       message = "#{source}: #{message}"
-      data = { :service_key => @api_key, :event_type => 'trigger', :description => message }
-     
+
+      data = { :service_key => @api_key, :incident_key => id, :event_type => event_type, :description => message }
       RestClient.post ENDPOINT, data.to_json, :content_type => :json
     end
+    
+    def send_notice(data)
+      contact_pagerduty(:trigger, data)
+    end
     alias :send_alert :send_notice
-    alias :send_resolve :send_notice
+
+    def send_resolve(data)
+      contact_pagerduty(:resolve, data)
+    end
   end
 end
