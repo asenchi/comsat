a235a1c92ee5cba755220b5cfb4035ae91eb2998
Curt Micol <asenchi+github@asenchi.com>
2014-05-29 11:39:04 -0700

    Spike out GitHub service

--- lib/comsat/services/github.rb
+++ lib/comsat/services/github.rb
@@ -1,3 +1,4 @@
+# GitHub Comsat URI: github://<api_key>:X@github.com/user/repo
 module Comsat
   class Github < Service::Base
     def send_notice(data)
@@ -9,5 +10,33 @@ module Comsat
 
     private
 
+    def github_token
+      @credentials.api_key
+    end
+
+    def github_repo
+      @credentials.scope
+    end
+
+    def github_client
+      @client = Octokit::Client.new(:access_token => github_token)
+    end
+
+    def create_issue(data)
+      title = "[#{data[:source]}] #{data[:message_id]}"
+      body = "\n#{data[:message]}\n"
+      github_client.create_issue(github_repo, title, body)
+    end
+
+    def update_issue(issue, data)
+      body = "[#{data[:source]}] #{data[:message_id]}\n#{data[:message]}"
+      github_client.add_comment(github_repo, issue, body)
+    end
+
+    def close_issue(issue, data)
+      body = "[#{data[:source]}] #{data[:message_id]}\n#{data[:message]}"
+      github_client.add_comment(github_repo, issue, body)
+      github_client.close_issue(github_repo, issue)
+    end
   end
 end
