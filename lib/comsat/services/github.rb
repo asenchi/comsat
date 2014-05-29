729324e62e2eeb5130b62e253ed7a7b07808d9df
Curt Micol <asenchi+github@asenchi.com>
2014-05-29 12:07:40 -0700

    Flesh out notice and resolve

--- lib/comsat/services/github.rb
+++ lib/comsat/services/github.rb
@@ -2,10 +2,30 @@
 module Comsat
   class Github < Service::Base
     def send_notice(data)
+      issue = github_client.issues(github_repo, {
+        :state => 'open',
+      }).select { |i|
+        i.title.start_with? issue_title(data)
+      }.first
+
+      if issue.nil?
+        create_issue(data)
+      else
+        update_issue(data)
+      end
     end
     alias :send_alert :send_notice
 
     def send_resolve(data)
+      issue = github_client.issues(github_repo, {
+        :state => 'open',
+      }).select { |i|
+        i.title.start_with? issue_title(data)
+      }.first
+
+      unless issue.nil?
+        close_issue(issue.number, data)
+      end
     end
 
     private
@@ -22,14 +42,26 @@ module Comsat
       @client = Octokit::Client.new(:access_token => github_token)
     end
 
+    def issue_title(data)
+      "[#{data[:source]}] #{data[:message_id]}"
+    end
+
+    def issue_body(data)
+      "\n#{data[:message]}\n"
+    end
+
+    def issue_comment(data)
+      "[#{data[:source]}] #{data[:message_id]}\n#{data[:message]}"
+    end
+
     def create_issue(data)
-      title = "[#{data[:source]}] #{data[:message_id]}"
-      body = "\n#{data[:message]}\n"
+      title = issue_title(data)
+      body  = issue_body(data)
       github_client.create_issue(github_repo, title, body)
     end
 
     def update_issue(issue, data)
-      body = "[#{data[:source]}] #{data[:message_id]}\n#{data[:message]}"
+      body = issue_comment(data)
       github_client.add_comment(github_repo, issue, body)
     end
 
